bec8cc2e4e6f790b79d5ae09938d3552846b7dea
dkssudghejr <dkssudghejr@naver.com>
2023-07-03 17:29:53 +0900

    관리자 상품상세페이지

--- js/productForm.js
+++ js/productForm.js
@@ -2,3 +2,8 @@ $("#file").on('change',function(){
     var fileName = $("#file").val();
     $(".upload-name").val(fileName);
   });
+
+var cateBoxChange = function(value){
+  console.log("값변경 테스트 : " + value);
+  $("#cate").val(value);
+}
